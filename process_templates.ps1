28ee9ef5b5b6d86bdeb7003b5878c97a5d807ba1
Martin Zuther <code@mzuther.de>
2022-12-15 08:27:06 +0100

    add PowerShell scripts

---
+++ process_templates.ps1
@@ -0,0 +1,2 @@
+# process all templates
+python ".\src\StempelWerk.py" "..\settings_example.json"
---
+++ update_templates.ps1
@@ -0,0 +1,2 @@
+# process all templates
+python ".\src\StempelWerk.py" --only-modified "..\settings_example.json"
