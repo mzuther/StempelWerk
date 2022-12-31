f898761f788f18b1f91563996ce399b485467937
Martin Zuther <code@mzuther.de>
2022-12-31 13:35:48 +0100

    add scripts to help with debugging

---
+++ debug.ps1
@@ -0,0 +1,2 @@
+# process all templates and print debug messages
+python -m src.StempelWerk --debug "../settings_example.json"
---
+++ debug.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+# process all templates and print debug messages
+python3 -m src.StempelWerk --debug "../settings_example.json"
