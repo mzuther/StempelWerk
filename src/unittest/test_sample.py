ff00cca14ba66b997013127b8ab6d82c06151c63
Martin Zuther <code@mzuther.de>
2023-01-14 21:11:34 +0100

    group sample tests in class

--- src/unittest/test_sample.py
+++ src/unittest/test_sample.py
@@ -5,9 +5,10 @@ def inc(x):
     return x + 1
 
 
-def test_increment_integer():
-    assert inc(3) == 4
+class TestSample:
+    def test_increment_integer(self):
+        assert inc(3) == 4
 
 
-def test_increment_float():
-    assert inc(3.14) == pytest.approx(4.14)
+    def test_increment_float(self):
+        assert inc(3.14) == pytest.approx(4.14)
