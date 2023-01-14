82c11d7a2bf9b2272d8fa2c83c4db781944f405f
Martin Zuther <code@mzuther.de>
2023-01-14 20:37:42 +0100

    add sample unit tests using "pytest"

---
+++ src/unittest/test_sample.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def inc(x):
+    return x + 2
+
+
+def test_increment_integer():
+    assert inc(3) == 4
+
+
+def test_increment_float():
+    assert inc(3.14) == pytest.approx(4.14)
