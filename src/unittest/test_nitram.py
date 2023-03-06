a53af7242da077e08182b1dc7fd1055441d9c89a
Martin Zuther <code@mzuther.de>
2023-03-06 22:16:11 +0100

    test lean removal of template files

--- src/unittest/test_nitram.py
+++ src/unittest/test_nitram.py
@@ -99,3 +99,44 @@ class TestNitram(TestCommon):
 
         # full run also renders externally modified output files
         convenience_run(partial_run=False, matching=True)
+
+
+
+    def test_lean_template_removal(self, tmp_path):
+
+        def convenience_run(partial_run, matching):
+            self.run(
+                config_path,
+                process_only_modified=partial_run)
+
+            if matching:
+                self.compare_directories(config)
+            else:
+                with pytest.raises(AssertionError):
+                    self.compare_directories(config)
+
+
+        def remove_file(partial_file_path):
+            output_path = os.path.join(tmp_path, partial_file_path)
+            os.remove(output_path)
+
+        # ---------------------------------------------------------------------
+
+        config = {
+            'stencil_dir_name': 'stencils',
+        }
+
+        config_path = self.create_config(
+            config, tmp_path, 'settings.json')
+
+        unit_test_directory = '1_process_only_modified_1'
+
+        with open(config_path, mode='r') as f:
+            config = json.load(f)
+
+        # set up StempelWerk and execute full run
+        self.run_and_compare(config_path, unit_test_directory)
+
+        # deleting a template leaves the output file alone
+        remove_file('10-templates/ab.jinja')
+        convenience_run(partial_run=False, matching=True)
