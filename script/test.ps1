4189fdfda988bae43b9c12cb2ac02f8d21c9721a
Martin Zuther <code@mzuther.de>
2024-01-28 19:43:33 +0100

    run slow tests when all other tests have passed

--- script/test.ps1
+++ script/test.ps1
@@ -1,6 +1,17 @@
 Write-Output ""
 
-. "$env:APPDATA\Python\Scripts\poetry.exe" run python -m pytest --ignore=".\.git" $args
+# run quick tests first
+. "$env:APPDATA\Python\Scripts\poetry.exe" run python -m pytest --ignore=".\.git" -m "not slow" $args
+
+If (-Not $?)
+{
+    exit
+}
+
+Write-Output ""
+
+# run slow tests only when all other tests have passed
+. "$env:APPDATA\Python\Scripts\poetry.exe" run python -m pytest --ignore=".\.git" -m "slow" $args
 
 If (-Not $?)
 {
